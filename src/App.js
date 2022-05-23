a4825ac08a425f4b4a0c21d5b17764de4c496eaf
Diego Echalar Penailillo <diegoechalarp@hotmail.com>
2022-05-23 13:21:56 -0400

    Actualizado 13:20

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
-import freeCodeCampLogo from './Imagenes/fcc_image.png' 
+import Boton from './componentes/Boton';
+import Contador from './componentes/contador';
+import freeCodeCampLogo from './Imagenes/fcc_image.png';
+import { useState } from 'react';
 
 
 function App() {
+
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  };
+
+  const reiniciarContador = () => {
+    setNumClics(numClics - 1);
+  };
+
   return (
     <div className="App">
       <div className='freecodecamp-logo-contenedor'>
@@ -11,10 +25,17 @@ function App() {
           src={freeCodeCampLogo}
           alt='Logo de freeCodeCamp' />
       </div>
-        <div className='contenedor-principal'>
-        
+      <div className='contenedor-principal'>
+        <Contador numClics={numClics} />
+        <Boton
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic} />
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador} />
         </div>
-
     </div>
   );
 }
